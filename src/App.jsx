dc133170f8735e7dc648c3519de3efe4ad2353eb
M Awais <khanmowais5@gmail.com>
2025-12-10 20:40:20 +0500

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import './App.css'
 import { LoadingScreen } from './Components/LoadingScreen'
 import "./index.css"
 import { useState, useEffect } from "react";
-
+import {Navbar} from './Components/Sections/Navbar';
 function App() {
  
  const [isloaded,setIsloaded]=useState(false);
+ const [menuOpen,setMenuOpen]=useState(false);
   return (
     <>
-    {!isloaded && <LoadingScreen onComplete={()=>setIsloaded(true)}/>}
+    {!isloaded && <LoadingScreen onComplete={()=>setIsloaded(true)}/>}{""}
+      <div className={`min-h-screen transition-opacity duration-700 ${isloaded ? "opacity-100" : "opacity-0"} 
+      bg-black text-gray-100`}></div>
+      <Navbar menuOpen={menuOpen} setMenuOpen={menuOpen}/>
     </>
   )
 }
